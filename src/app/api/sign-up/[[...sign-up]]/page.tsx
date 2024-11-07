9b6adaf7fd6d3d6b7c2e22041159686b54b00077
Renuka Kulkarni <renuka.kulkarni7087@gmail.com>
2024-11-07 17:27:59 -0500

    styling

--- src/app/api/sign-up/[[...sign-up]]/page.tsx
+++ src/app/api/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,5 @@
 import { SignUp } from '@clerk/nextjs'
 
 export default function Page() {
-  return <SignUp />
+  return  <div className="absolute inset-0 flex items-center justify-center"><SignUp /> </div>
 }
