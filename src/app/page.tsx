0df158933bdbc8a9ec6adcd67e72f6bb6a723dd6
Renuka Kulkarni <renuka.kulkarni7087@gmail.com>
2024-11-07 17:22:54 -0500

    intigrated clerk authentiation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Link from "next/link";
 
 import { LatestPost } from "~/app/_components/post";
+import { Button } from "~/components/ui/button";
 import { api, HydrateClient } from "~/trpc/server";
 
 export default async function Home() {
   return (
-    <h1>Hello</h1>
+    <Button>Hello</Button>
   );
 }
